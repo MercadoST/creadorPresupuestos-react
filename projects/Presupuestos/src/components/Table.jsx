25001b4f98a8c5d7b2fad3c35fd1b549dd6ff527
LuisLuna810 <luis.agustin10@hotmail.com>
2023-06-23 09:12:09 -0300

    23/06/23

--- projects/Presupuestos/src/components/Table.jsx
+++ projects/Presupuestos/src/components/Table.jsx
@@ -1,28 +1,30 @@
-import { useState } from 'react';
-import { VirtualMachine } from './VirtualMachine';
-import { Categories } from './Categories';
-
+import { useState } from "react";
+import { VirtualMachine } from "./VirtualMachine";
+import { Categories } from "./Categories";
 
 export function Table() {
   const [count, setCount] = useState(1);
 
   const handleClick = () => {
     setCount(count + 1);
+    console.log(components.length+1);
   };
 
-  const components = new Array(count).fill().map((_, index) => (
-    <VirtualMachine key={index + 1} vm={index + 1}/>
-  ));
-
+  const components = new Array(count)
+    .fill()
+    .map((_, i) => (
+      <VirtualMachine handleClick={handleClick} key={i + 1} vm={i + 1} />
+    ));
 
   return (
     <div>
-      <table className='bg-light container-fluid rounded'>
-      <Categories />
-      {components}
-    </table>
-    <button className='btn btn-primary center m-3 ' onClick={handleClick}>Agregar</button>
+      <table className="bg-light container-fluid rounded">
+        <Categories />
+        {components}
+      </table>
+      <button className="btn btn-primary center m-3 " onClick={handleClick}>
+        Agregar
+      </button>
     </div>
-    
   );
 }
--- projects/Presupuestos/src/components/VirtualMachine.jsx
+++ projects/Presupuestos/src/components/VirtualMachine.jsx
@@ -3,6 +3,7 @@ import deleteIcon from "../resources/bote-de-basura.png";
 
 export function VirtualMachine({ vm }) {
   const [products, setProducts] = useState([]);
+  const [display, setDisplay] = useState(true);
 
   useEffect(() => {
     const fetchAllProducts = async () => {
@@ -16,37 +17,45 @@ export function VirtualMachine({ vm }) {
   const categorias = Array.from({ length: 14 }, (_, i) => i + 1);
 
 
-  return (
-    <tbody>
-      <tr>
-        <th className="text-center">{`VM ${vm}`}</th>
-        {categorias.map((categoria) => (
-          <th key={categoria}>
-            <select className="form-select">
-              {products.map(producto => {
-                if (producto.id_Category === categoria) {
-                  return (
-                    <option key={producto.id} value={producto.nombre}>
-                      {producto.nombre}
-                    </option>
-                  );
-                } else {
-                  return null
-                }
-              })}
-            </select>
-          </th>
-        ))}
-        <td className="text">
-          <button className="btn btn-outline-danger btn-1">
-            <img
-              style={{ width: "1.4rem", height: "1.4rem" }}
-              src={deleteIcon}
-              alt="deleteIcon"
-            />
-          </button>
-        </td>
-      </tr>
-    </tbody>
-  );
+  const handleDelete = () => {
+    setDisplay(false);
+  };
+
+  if (!display) {
+    return null;
+  } else {
+    return (
+      <tbody>
+        <tr>
+          <th className="text-center">{`VM ${vm}`}</th>
+          {categorias.map((categoria) => (
+            <th key={categoria}>
+              <select className="form-select">
+                {products.map((producto) => {
+                  if (producto.id_Category === categoria) {
+                    return (
+                      <option key={producto.id} value={producto.nombre}>
+                        {producto.nombre}
+                      </option>
+                    );
+                  } else {
+                    return null;
+                  }
+                })}
+              </select>
+            </th>
+          ))}
+          <td className="text">
+            <button onClick={handleDelete} className="btn btn-outline-danger btn-1">
+              <img
+                style={{ width: "1.4rem", height: "1.4rem" }}
+                src={deleteIcon}
+                alt="deleteIcon"
+              />
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    );
+  }
 }
